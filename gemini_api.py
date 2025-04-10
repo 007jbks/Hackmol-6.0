25821f1415e7f23f22eb090c8e2122468ef516ca
Kartik Sirohi <kartiksirohi383@gmail.com>
2025-04-10 17:44:51 +0530

    API key changed

--- gemini_api.py
+++ gemini_api.py
@@ -3,7 +3,7 @@ import json
 import base64
 import re
 
-API_KEY = "AIzaSyDfRLr5B0vgLeMhIf0MmnRnjBd8vJ9W08Y"
+API_KEY = "AIzaSyC_WQRG1JmV42vlZfA04gRb3IGj3KgpZqI"
 URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
 
 # Convert image URL to base64
